7de28725164df3f32502c1c692a81736b79cd678
myapp01 <thanawatpalaket9090@gmail.com>
2021-11-05 11:38:58 +0700

    initial commit

---
+++ phpapp.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta chaeset="utf8">
+    <meta name="viewport" content="width=device-width -device, initial-scale=1.0">
+    <title>PHP: Basic<title>
+    <style>
+        h1{
+            color:red;
+        }
+    </style>    
+</head>
+
+<body>
+    <h1 style="color:blueviolet;">
+        <?php echo "PHP Syntax"; ?>
+    </h1>
+</body>
+
+</html>
